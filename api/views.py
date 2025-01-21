2ff98287ddd37d09cf3edcca3eb1cfc2dcaabee3
danielcorte <daniellcorte@gmail.com>
2025-01-21 16:03:08 -0300

    criação do metodo post para professores

--- api/views.py
+++ api/views.py
@@ -4,14 +4,24 @@ from .serializer import CadastroSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListCreateAPIView
+from rest_framework import status
 
 @api_view(['GET', 'POST'])
 def listar_professores(request):
     if request.method == 'GET':
         queryset = Cadastro.objects.all()
         serializer = CadastroSerializer(queryset, many=True)
-
-    return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = CadastroSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ProfessoresView(ListCreateAPIView):
-    
+    queryset = Cadastro.objects.all()
+    serializer_class = CadastroSerializer
